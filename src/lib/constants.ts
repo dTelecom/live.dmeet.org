b9751b91da84e54426bb56a655c7fbef21edb3ef
Roman <email>
2025-05-05 17:35:10 +0300

    change reward amount

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 import { LocalUserChoices } from '@dtelecom/components-react';
 
-export const ADMIN_POINTS_MULTIPLIER = 2;
-export const BASE_REWARDS_PER_MINUTE = 10;
+export const ADMIN_POINTS_MULTIPLIER = 4;
+export const BASE_REWARDS_PER_MINUTE = 5;
 export const REFERRAL_REWARD_PERCENTAGE = 10;
 
 export const defaultPreJoinChoices: Partial<LocalUserChoices> = {
